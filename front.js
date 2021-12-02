3ca485eed0e6a86bccd6b21faf86ed370c45fb58
Joe <schirmer.pc@gmail.com>
2021-12-02 11:21:04 -0500

    text edit

--- front.js
+++ front.js
@@ -13,7 +13,7 @@ export function frontPage (){
     //const btn = document.createElement('button');
     
     elementA.className = 'text';
-    elementA.innerHTML = 'a wonderful place to grab a byte. dont miss the spaghetti 0s. blah blah blah blahdibbyd da da. Joes was started in 2021. shortly after covid was offically canceled by those elibiable to cancel(individuals known as: "insert chosen pronoun here"). It was meant to be a place simply to attain qualifications for government handouts. then one day joes famous ramen recipe was released via hacker document dumps and the rest is history'; 
+    elementA.innerHTML = 'a wonderful place to grab a byte. dont miss the spaghetti 0s. blah blah blah blahdibbyd da da. Joes was started in 2021. shortly after covid was offically canceled by those elibiable to cancel(individuals known as: "insert chosen pronoun here"). It was meant to be a place simply for joes friends to eat. Then one day joes famous ramen recipe was released via hacker document dumps and the rest is so called history'; 
     frontContent.appendChild(elementA);
 
     elementB.className = "title"
